9f9cc1b9d44f1c0f1089b486bab60ad03d875a3c
davidscales7 <davidscales7@gmail.com>
2026-01-12 20:35:17 +0000

    making the page live

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/davidscales7.github.io',
+  assetPrefix: '/davidscales7.github.io/',
+}
 
 export default nextConfig;
